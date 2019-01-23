29af44f7545471d071d8bc3de84773d1260d3fa5
NdumisoMthembu <mrnnmthembu@gmail.com>
2019-01-24 00:37:01 +0200

    new

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      NavigationComponent,
+      HomePageComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
